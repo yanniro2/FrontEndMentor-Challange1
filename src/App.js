3f2e2960b5ad5e3bcb66c88ab62926757d545275
Niroyan Kugaruban <niroyan.kugaruban@anjn.com.au>
2022-06-22 16:16:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./Style/Main.css";
+import iconStar from "./Img/icon-star.svg";
+import ilustration from "./Img/illustration-thank-you.svg";
+import React from "react";
 function App() {
+  const [value, setValue] = React.useState(false);
+  const [display, setDisplay] = React.useState(false);
+  const onClickhandele = (event) => {
+    event.preventDefault();
+    setValue(event.target.value);
+  };
+
+  function submit() {
+    setDisplay(!display);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {display ? (
+        <div className="submit-before">
+          <img src={ilustration} alt="ilustration" />
+          <div className="message">
+            <p>You Selected {value} out of 5</p>
+          </div>
+          <h3>Thank you </h3>
+          <p>
+            We appreciate you taking the time to give a rating. If you ever need
+            more support, don’t hesitate to get in touch!
+          </p>
+        </div>
+      ) : (
+        <div className="default">
+          <div className="icon">
+            <img src={iconStar} alt="Icon" />
+          </div>
+          <h3>How did we do?</h3>
+          <p>
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering!
+          </p>
+          <div className="number">
+            <button
+              className="num"
+              value="1"
+              title="num1"
+              onClick={onClickhandele}
+            >
+              1
+            </button>
+            <button
+              className="num"
+              value="2"
+              title="num2"
+              onClick={onClickhandele}
+            >
+              2
+            </button>
+            <button
+              className="num "
+              value="3"
+              title="num3"
+              onClick={onClickhandele}
+            >
+              3
+            </button>
+            <button
+              className="num "
+              value="4"
+              title="num4"
+              onClick={onClickhandele}
+            >
+              4
+            </button>
+            <button
+              className="num "
+              value="5"
+              title="num5"
+              onClick={onClickhandele}
+            >
+              5
+            </button>
+          </div>
+          <button className="Submit" onClick={submit}>
+            Submit
+          </button>
+        </div>
+      )}
     </div>
   );
 }
